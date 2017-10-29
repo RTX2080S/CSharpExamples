02951970ae6febd41a787a0e76b6ec1e4ed5f4db
LL <nzlili2015@gmail.com>
2017-10-30 05:49:22 +0800

    create CancellationToken example

---
+++ CancellationTokenExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancellationTokenExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken token = cancellationTokenSource.Token;
+            Task task = Task.Run(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    Console.Write("*");
+                    Thread.Sleep(1000);
+                }
+            }, token);
+            Console.WriteLine("Press enter to stop the task");
+            Console.ReadLine();
+            cancellationTokenSource.Cancel();
+            Console.WriteLine("Press enter to end the application");
+            Console.ReadLine();
+        }
+    }
+}
