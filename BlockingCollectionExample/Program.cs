689da62a4d137294549069e99681d4890034f6cf
LL <nzlili2015@gmail.com>
2017-10-30 04:57:53 +0800

    Add more examples for concurrent collection

---
+++ BlockingCollectionExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace BlockingCollectionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockingCollection<string> col = new BlockingCollection<string>();
+            Task read = Task.Run(() =>
+            {
+                // This would be the same with GetConsumingEnumerable()
+                //while (true)
+                //    Console.WriteLine(col.Take());
+
+                // This would be the same with the above while statement
+                foreach (string v in col.GetConsumingEnumerable())
+                    Console.WriteLine(v);
+            });
+            Task write = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string s = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(s)) break;
+                    col.Add(s);
+                }
+            });
+            write.Wait();
+        }
+    }
+}
---
+++ EnumeratingConcurrentBag/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EnumeratingConcurrentBag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            Task.Run(() =>
+            {
+                bag.Add(42);
+                Thread.Sleep(1000);
+                bag.Add(21);
+            });
+            Task.Run(() =>
+            {
+                // Pause for a short while to allow for 42 added into the bag
+                Thread.Sleep(10);
+                foreach (int i in bag)
+                    Console.WriteLine(i);
+            }).Wait();
+
+            Console.ReadKey();
+            // Displays
+            // 42
+            // This code only displays 42 because the other value is added after iterating over the bag has started
+        }
+    }
+}
