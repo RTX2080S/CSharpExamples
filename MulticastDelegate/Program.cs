98be7517c8049c209f1ac1e3779c7c61d1bc9a58
LL <nzlili2015@gmail.com>
2017-10-30 07:03:17 +0800

    Create the examples of delegates

---
+++ MulticastDelegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace MulticastDelegateExample
+{
+    class Program
+    {
+        delegate void Del();
+
+        static void Add()
+        {
+            Console.WriteLine("Method 1");
+        }
+
+        static void Multiply()
+        {
+            Console.WriteLine("Method 2");
+        }
+
+        static void Main(string[] args)
+        {
+            Del del = Add;
+            del += Multiply;
+
+            int invocationCount = del.GetInvocationList().GetLength(0);
+            Console.WriteLine($"{invocationCount} methods are registered.");
+
+            Console.WriteLine("Press any key to invoke...");
+            Console.ReadKey();
+            del();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UisngDelegate/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace UisngDelegate
+{
+    class Program
+    {
+        delegate int Calculate(int x, int y);
+
+        static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+            Calculate calc = Add;
+            Console.WriteLine(calc(3, 4)); // Displays 7
+            calc = Multiply;
+            Console.WriteLine(calc(3, 4)); // Displays 12
+            // Using lambda
+            calc = (x, y) => x - y;
+            Console.WriteLine(calc(3, 4)); // Displays -1
+            calc = (x, y) => { return x + y; };
+            Console.WriteLine(calc(3, 4)); // Displays 7
+            Console.ReadKey();
+        }
+    }
+}
