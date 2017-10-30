2fe26d2d4b53e585de699c4c7b3c3b3ac4f400f3
LL <nzlili2015@gmail.com>
2017-10-30 11:29:13 +0800

    Create implicit and explicit example

---
+++ ImplicitExplicitConversion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ImplicitExplicitConversion
+{
+    class Money
+    {
+        public Money(decimal amount)
+        {
+            Amount = amount;
+        }
+
+        public decimal Amount { get; set; }
+
+        public static implicit operator decimal(Money money)
+        {
+            return money.Amount;
+        }
+
+        public static explicit operator int(Money money)
+        {
+            return (int)money.Amount;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Money m = new Money(42.42M);
+            decimal amount = m;
+            int truncatedAmount = (int)m;
+            Console.ReadKey();
+        }
+    }
+}
