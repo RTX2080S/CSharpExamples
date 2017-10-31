e0bfbbec8d7c38da7e364f9a750ee8289fc1415b
LL <nzlili2015@gmail.com>
2017-11-01 06:19:36 +0800

    Create example for flag

---
+++ FlagAttributeExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FlagAttributeExample
+{
+    enum Suits
+    {
+        Spades = 1,
+        Clubs = 2,
+        Diamonds = 4,
+        Hearts = 8
+    }
+
+    [Flags]
+    enum SuitsFlags
+    {
+        Spades = 1,
+        Clubs = 2,
+        Diamonds = 4,
+        Hearts = 8
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str1 = (Suits.Spades | Suits.Diamonds).ToString();
+            Console.WriteLine(str1);    // "5"
+            var str2 = (SuitsFlags.Spades | SuitsFlags.Diamonds).ToString();
+            Console.WriteLine(str2);    // "Spades, Diamonds"
+            Console.ReadKey();
+        }
+    }
+}
