67b74a08df844cb58cb76496383ed5142a5cf0a9
LL <52593730@qq.com>
2017-11-09 23:36:10 +1300

    create data file

---
+++ FileStreamDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStreamDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"test.dat";
+            using (FileStream fileStream = File.Create(path))
+            {
+                string myValue = "MyValue";
+                byte[] data = Encoding.UTF8.GetBytes(myValue);
+                fileStream.Write(data, 0, data.Length);
+            }
+
+            using (FileStream fileStream = File.OpenRead(path))
+            {
+                byte[] data = new byte[fileStream.Length];
+                for (int index = 0; index < fileStream.Length; index++)
+                {
+                    data[index] = (byte)fileStream.ReadByte();
+                }
+                Console.WriteLine(Encoding.UTF8.GetString(data)); // Displays: MyValue
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
