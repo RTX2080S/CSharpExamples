94ac4a091867182c0d9c081a731ea4ad6a2ae568
LL <52593730@qq.com>
2017-11-09 22:57:16 +1300

    add event log samples

---
+++ EventLogExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace EventLogExample
+{
+    class MySample
+    {
+        private const string EventLogSource = "EventLog Demo App";
+
+        public static void Main()
+        {
+            if (!EventLog.SourceExists(EventLogSource))
+            {
+                EventLog.CreateEventSource(EventLogSource, "MyNewLog");
+                Console.WriteLine("CreatedEventSource");
+                Console.WriteLine("Please restart application");
+                Console.ReadKey();
+                return;
+            }
+            EventLog myLog = new EventLog();
+            myLog.Source = EventLogSource;
+            myLog.WriteEntry("Log event!");
+        }
+    }
+}
